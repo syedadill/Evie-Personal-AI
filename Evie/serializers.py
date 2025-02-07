e98ff21ead67d2eb57aa23d8e544513ab399b14e
syedadill <adilfazal03@gmail.com>
2025-02-07 15:10:07 +0500

    User data fields3

--- Evie/serializers.py
+++ Evie/serializers.py
@@ -45,6 +45,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
 
 class PDFUploadSerializer(serializers.ModelSerializer):
     query = serializers.CharField(write_only=True)
